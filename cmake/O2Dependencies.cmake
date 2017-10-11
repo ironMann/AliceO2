0726280d912aad4e2f8b2535fb22fbe2dbb87c4d
Gvozden Neskovic <neskovic@compeng.uni-frankfurt.de>
2017-10-12 00:00:57 +0200

    DataDistribution: Readout and SubTimeFrame Building chain
    FLP chain: Building SubTimeFrames consisting of readout data
    using FairMQUnmanagedRegion
    
    Components:
      - Readout O2 Device (with a CRU emulator)
      - SubTimeFrameBuilder O2 Device
      - SubTimeFrameTransporter O2 Device
    
    STF Serialization options:
      1) header and data interleaved: each logical/physical data
         block is preceded by a header.
      2) headers first: all headers are sent first, then all
         physical data blocks.
    
    Signed-off-by: Gvozden Neskovic <neskovic@compeng.uni-frankfurt.de>

--- cmake/O2Dependencies.cmake
+++ cmake/O2Dependencies.cmake
@@ -75,6 +75,18 @@ endif()
 
 ########## Bucket definitions ############
 o2_define_bucket(
+    NAME
+    DataDistributionCommon_bucket
+
+    DEPENDENCIES
+    Headers
+    O2Device
+
+    INCLUDE_DIRECTORIES
+    ${CMAKE_SOURCE_DIR}/Utilities/DataDistribution/Common/include
+)
+
+o2_define_bucket(
   NAME
   glfw_bucket
 
